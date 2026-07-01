9343578c63c83bbdb643221df0a73ed8d6dc153c
Matiaaaamw <matiamorales87@gmail.com>
2026-07-01 15:05:48 -0400

    Update PRUEBA PARCIAL 4.py

--- PRUEBA PARCIAL 4.py
+++ PRUEBA PARCIAL 4.py
@@ -1,1 +1,12 @@
-aksdjhoiwahodhausbdhuoigit
+nombre = str(input("Ingrese su nombre"))
+edad = int(input("Ingrese su edad"))
+temperatura = float(input("Ingrese su temperatura actual"))
+
+print "========== MENÚ PRINCIPAL =========="
+print"1. Agregar paciente"
+print"2. Buscar paciente"
+print"3. Eliminar paciente"
+print"4. Actualizar estado"
+print "5. Mostrar pacientes"
+print"6. Salir"
+print "====================================="
