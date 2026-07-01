f77a0aa4c7096bb45469876e438fd0852730fdc5
Matiaaaamw <matiamorales87@gmail.com>
2026-07-01 15:46:03 -0400

    Update PRUEBA PARCIAL 4.py

--- PRUEBA PARCIAL 4.py
+++ PRUEBA PARCIAL 4.py
@@ -1,6 +1,53 @@
-nombre = str(input("Ingrese su nombre"))
-edad = int(input("Ingrese su edad"))
-temperatura = float(input("Ingrese su temperatura actual"))
+print ("Bienvenido al sistema de la clinica")
+print ("Porfavor, seleccione una opcion")
+
+print ("========== MENÚ PRINCIPAL ==========")
+print ("1. Agregar paciente")
+print("2. Buscar paciente")
+print ("3. Eliminar paciente")
+print ("4. Actualizar estado")
+print ("5. Mostrar pacientes")
+print ("6. Salir")
+print ("=====================================")
+
+
+opcion = int(input("Ingrese una opcion"))
+paciente = 0
+
+def opcion1
+while True:
+  nombre = str(input("Ingrese su nombre"))
+  edad = int(input("Ingrese su edad"))
+  temperatura = float(input("Ingrese su temperatura actual"))
+  if temperatura < 35.0 
+    print ("Ingrese una temperatura dentro del rango valido (35 - 42)")
+else temperatura > 42.0
+    print ("Ingrese una temperatura dentro del rango valido (35 - 42)")
+
+  return opcion1
+else:
+continue
+def (opcion2)
+
+
+
+
+def (opcion5)
+print ("=== LISTA DE PACIENTES ===")
+print (f"Nombre: {nombre}")
+print (f"Edad: {edad}")
+print (f"Temperatura: {temperatura}")
+print (f"Estado: {estado}")
+print ("********************************************")
+print (f"Nombre: {nombre}")
+print (f"Edad: {edad}")
+print (f"Temperatura: {temperatura}")
+print (f"Estado: {estado}")
+print ("*********************************************")
+  
+
+                   
+
 
 print "========== MENÚ PRINCIPAL =========="
 print"1. Agregar paciente"
